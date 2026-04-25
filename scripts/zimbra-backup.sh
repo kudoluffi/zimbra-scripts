f1059336cfa16fa16c3ab575619a0a4433962cd8
kudoluffi <kudo.luffi@yahoo.com>
2026-04-25 13:17:37 +0700

    Update zimbra-backup.sh
    menambahkan warna dan edit ke warna yang lebih terang biar mudah dibaca di terminal ubuntu

--- scripts/zimbra-backup.sh
+++ scripts/zimbra-backup.sh
@@ -5,7 +5,8 @@
 
 set -u
 
-RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'; BLUE='\033[0;34m'; NC='\033[0m'
+#RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'; BLUE='\033[0;34m'; NC='\033[0m'
+RED='\033[1;31m'; GREEN='\033[1;32m'; YELLOW='\033[1;33m'; BLUE='\033[1;34m'; MAGENTA='\033[1;35m'; CYAN='\033[1;36m'; WHITE='\033[1;37m'; NC='\033[0m'
 
 log()  { echo -e "${BLUE}[INFO]${NC} $1"; }
 pass() { echo -e "${GREEN}[PASS]${NC} $1"; }
