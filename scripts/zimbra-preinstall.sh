c8be34574cb4e69f626d31378384d37097e4acf9
kudoluffi <kudo.luffi@yahoo.com>
2026-08-19 16:39:33 +0700

    Allow TCP traffic on port 465 for email submission
    Added firewall rule to allow TCP traffic on port 465 for secure email submission.

--- scripts/zimbra-preinstall.sh
+++ scripts/zimbra-preinstall.sh
@@ -45,7 +45,7 @@ log "Updating system & installing core dependencies..."
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y \
-  dnsutils net-tools sysstat unzip pax sqlite3 perl libperl5.34 libdbi-perl \
+  dnsutils net-tools sysstat unzip pax sqlite3 perl libdbi-perl \
   libnet-dns-perl libexpat1 libssl-dev libxml2-dev libgomp1 libpq5 libpcre2-8-0
 
 # Install optional/legacy packages safely
@@ -143,6 +143,7 @@ ufw allow 80/tcp
 ufw allow 110/tcp
 ufw allow 143/tcp
 ufw allow 443/tcp
+ufw allow 465/tcp
 ufw allow 587/tcp
 ufw allow 993/tcp
 ufw allow 995/tcp
